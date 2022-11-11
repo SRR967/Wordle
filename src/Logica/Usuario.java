fbf9daa01ffe1cffbca176633cf175f72d4a0cb7
Santiago Ramon <santiramon967gmail.com>
2022-11-11 16:12:01 -0500

    Creacion clase usuario

---
+++ src/Logica/Usuario.java
@@ -0,0 +1,11 @@
+package Logica;
+
+public class Usuario {
+    private String nombreUsuario;
+    private int puntuacion;
+
+    public Usuario(String nombreUsuario, int puntuacion) {
+        this.nombreUsuario = nombreUsuario;
+        this.puntuacion = puntuacion;
+    }
+}
